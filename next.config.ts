52b85208118c3e321a1c4a0f775cfc9367146c81
Valentin Esquier <valentin.esquier@esq.group>
2026-04-19 18:58:57 +0200

    Add onboarding guide for new collaborators

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "https://fmcs.mykajabi.com/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
